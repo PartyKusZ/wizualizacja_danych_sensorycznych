15e4b9b6ce9f0bc5af28b8a2ad445292e9207708
Jakub Kusz <jakub.kusz@onet.pl>
2023-04-26 22:57:21 +0200

    nieudana próba połączenia sygnału i slotu

--- wizualizacja_silosow/aplikacja_qt/src/Main_window.cpp
+++ wizualizacja_silosow/aplikacja_qt/src/Main_window.cpp
@@ -1,5 +1,5 @@
 #include "Main_window.hpp"
-
+#include "signal_emiter.hpp"
  /**
  * @brief Construct a new Main_window object
  * 
@@ -9,8 +9,11 @@
 Main_window::Main_window(QWidget *parent): Ui::Main_window(), QWidget(parent){
    
     this->setupUi(this);
-    this->all_param_silos_1->set_values("2","1","3","7",98);
-    this->all_param_silos_2->set_values("2","1","3","7",50);
+    this->all_param_silos_1->set_values("25C","26C","30%","21%",98);
+    this->all_param_silos_2->set_values("25C","26C","30%","21%",50);
+    this->volume_info_al_1->setText("98%");
+    this->volume_info_al_2->setText("50%");
+   
 }   
 
 /**
