3fd9fe5834b606b878d4aef1d2693a00fdb08261
Jakub Kusz <jakub.kusz@onet.pl>
2023-05-28 00:35:58 +0200

    stworzenie pliku ts

---
+++ wizualizacja_silosow/aplikacja_qt/en.ts
@@ -0,0 +1,325 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_150" sourcelanguage="pl_PL">
+<context>
+    <name>All_param_backend</name>
+    <message>
+        <location filename="src/All_param_backend.cpp" line="57"/>
+        <location filename="src/All_param_backend.cpp" line="70"/>
+        <source>Temperatury w normie</source>
+        <translation>Normal temperatures</translation>
+    </message>
+    <message>
+        <location filename="src/All_param_backend.cpp" line="61"/>
+        <location filename="src/All_param_backend.cpp" line="74"/>
+        <location filename="src/All_param_backend.cpp" line="88"/>
+        <location filename="src/All_param_backend.cpp" line="101"/>
+        <location filename="src/All_param_backend.cpp" line="114"/>
+        <location filename="src/All_param_backend.cpp" line="127"/>
+        <source>Przekroczono wartość ostrzegawczą</source>
+        <translation>Warning value exceeded</translation>
+    </message>
+    <message>
+        <location filename="src/All_param_backend.cpp" line="65"/>
+        <location filename="src/All_param_backend.cpp" line="78"/>
+        <location filename="src/All_param_backend.cpp" line="92"/>
+        <location filename="src/All_param_backend.cpp" line="105"/>
+        <location filename="src/All_param_backend.cpp" line="118"/>
+        <location filename="src/All_param_backend.cpp" line="131"/>
+        <source>Przekroczono wartość krytyczną</source>
+        <translation>Critical value exceeded</translation>
+    </message>
+    <message>
+        <location filename="src/All_param_backend.cpp" line="84"/>
+        <location filename="src/All_param_backend.cpp" line="97"/>
+        <source>Wilgotność w normie</source>
+        <translation>Normal humidity</translation>
+    </message>
+    <message>
+        <location filename="src/All_param_backend.cpp" line="110"/>
+        <location filename="src/All_param_backend.cpp" line="123"/>
+        <source>Objętość w normie</source>
+        <translation>Volume normal</translation>
+    </message>
+</context>
+<context>
+    <name>Hum_backend</name>
+    <message>
+        <location filename="src/Hum_backend.cpp" line="21"/>
+        <source>Alarmy wilgotności - silos 1</source>
+        <translation>Humidity alarms - silo 1</translation>
+    </message>
+    <message>
+        <location filename="src/Hum_backend.cpp" line="22"/>
+        <source>Alarmy wilgotności - silos 2</source>
+        <translation>Humidity alarms - silo 2</translation>
+    </message>
+    <message>
+        <location filename="src/Hum_backend.cpp" line="63"/>
+        <location filename="src/Hum_backend.cpp" line="76"/>
+        <source>Wilgotność w normie</source>
+        <translation>Normal humidity</translation>
+    </message>
+    <message>
+        <location filename="src/Hum_backend.cpp" line="67"/>
+        <location filename="src/Hum_backend.cpp" line="80"/>
+        <source>Przekroczono wartość ostrzegawczą</source>
+        <translation>Warning value exceeded</translation>
+    </message>
+    <message>
+        <location filename="src/Hum_backend.cpp" line="71"/>
+        <location filename="src/Hum_backend.cpp" line="84"/>
+        <source>Przekroczono wartość krytyczną</source>
+        <translation>Critical value exceeded</translation>
+    </message>
+</context>
+<context>
+    <name>Main_window</name>
+    <message>
+        <location filename="ui/Main_window.ui" line="20"/>
+        <source>Wizualizacja Silosów</source>
+        <translation type="unfinished">Silos visualisation</translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="113"/>
+        <source>Wszystkie parametry</source>
+        <translation type="unfinished">All parameters</translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="144"/>
+        <location filename="ui/Main_window.ui" line="485"/>
+        <location filename="ui/Main_window.ui" line="1781"/>
+        <location filename="ui/Main_window.ui" line="1832"/>
+        <source>70%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="205"/>
+        <location filename="ui/Main_window.ui" line="549"/>
+        <location filename="ui/Main_window.ui" line="845"/>
+        <location filename="ui/Main_window.ui" line="1170"/>
+        <source>TEMPERATURA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="261"/>
+        <location filename="ui/Main_window.ui" line="367"/>
+        <location filename="ui/Main_window.ui" line="444"/>
+        <location filename="ui/Main_window.ui" line="610"/>
+        <location filename="ui/Main_window.ui" line="632"/>
+        <location filename="ui/Main_window.ui" line="761"/>
+        <location filename="ui/Main_window.ui" line="864"/>
+        <location filename="ui/Main_window.ui" line="1189"/>
+        <location filename="ui/Main_window.ui" line="1533"/>
+        <location filename="ui/Main_window.ui" line="1669"/>
+        <location filename="ui/Main_window.ui" line="1932"/>
+        <location filename="ui/Main_window.ui" line="2068"/>
+        <source>Przekroczono poziom(I II) </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="294"/>
+        <location filename="ui/Main_window.ui" line="699"/>
+        <source>OBJĘTOŚĆ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="425"/>
+        <location filename="ui/Main_window.ui" line="742"/>
+        <location filename="ui/Main_window.ui" line="1514"/>
+        <location filename="ui/Main_window.ui" line="1650"/>
+        <source>WILGOTNOŚĆ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="787"/>
+        <source>Temperatura</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="919"/>
+        <location filename="ui/Main_window.ui" line="1244"/>
+        <location filename="ui/Main_window.ui" line="1588"/>
+        <location filename="ui/Main_window.ui" line="1724"/>
+        <location filename="ui/Main_window.ui" line="1987"/>
+        <location filename="ui/Main_window.ui" line="2123"/>
+        <source>Ustawienia alarmów</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="958"/>
+        <location filename="ui/Main_window.ui" line="986"/>
+        <location filename="ui/Main_window.ui" line="1064"/>
+        <location filename="ui/Main_window.ui" line="1092"/>
+        <location filename="ui/Main_window.ui" line="1302"/>
+        <location filename="ui/Main_window.ui" line="1330"/>
+        <location filename="ui/Main_window.ui" line="1408"/>
+        <location filename="ui/Main_window.ui" line="1436"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="1267"/>
+        <source>Wilgotność</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="1747"/>
+        <location filename="ui/Main_window.ui" line="2197"/>
+        <source>Wypełnienie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="1913"/>
+        <location filename="ui/Main_window.ui" line="2049"/>
+        <source>WYPEŁNIENIE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2148"/>
+        <source>Dane historyczne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2160"/>
+        <source>Sios 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2167"/>
+        <source>Wybierz parametr do przedstawienia na wykresie </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2183"/>
+        <source>Silos 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2202"/>
+        <source>Temperatura 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2207"/>
+        <source>Temperatura 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2212"/>
+        <source>Wilgotność 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2217"/>
+        <source>wilgotność 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2228"/>
+        <source>Od:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2235"/>
+        <source>Zatwierdź</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2242"/>
+        <source>Wybierz silos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2249"/>
+        <source>Do:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Main_window.ui" line="2256"/>
+        <location filename="ui/Main_window.ui" line="2263"/>
+        <source>Pokaż kalendarz</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Temp_backend</name>
+    <message>
+        <location filename="src/Temp_backend.cpp" line="22"/>
+        <source>Alarmy temperatury - silos 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Temp_backend.cpp" line="23"/>
+        <source>Alarmy temperatury - silos 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Temp_backend.cpp" line="64"/>
+        <location filename="src/Temp_backend.cpp" line="77"/>
+        <source>Temperatury w normie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Temp_backend.cpp" line="68"/>
+        <location filename="src/Temp_backend.cpp" line="81"/>
+        <source>Przekroczono wartość ostrzegawczą</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Temp_backend.cpp" line="72"/>
+        <location filename="src/Temp_backend.cpp" line="85"/>
+        <source>Przekroczono wartość krytyczną</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Vol_backend</name>
+    <message>
+        <location filename="src/Vol_backend.cpp" line="21"/>
+        <source>Alarmy wypełnienia - silos 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Vol_backend.cpp" line="22"/>
+        <source>Alarmy wypełnienia - silos 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Vol_backend.cpp" line="61"/>
+        <location filename="src/Vol_backend.cpp" line="74"/>
+        <source>Wilgotność w normie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Vol_backend.cpp" line="65"/>
+        <location filename="src/Vol_backend.cpp" line="78"/>
+        <source>Przekroczono wartość ostrzegawczą</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Vol_backend.cpp" line="69"/>
+        <location filename="src/Vol_backend.cpp" line="82"/>
+        <source>Przekroczono wartość krytyczną</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>alarms_window</name>
+    <message>
+        <location filename="ui/Alarms_window.ui" line="14"/>
+        <source>ustaw nazwe w kodzie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Alarms_window.ui" line="48"/>
+        <source>Poziom ostrzeżenia [°C]:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Alarms_window.ui" line="129"/>
+        <source>Poziom krytyczny [°C]:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
